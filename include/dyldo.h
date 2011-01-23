40fed49a9c7d12e0e3e9dba1279dd7e55d5d1f95
Vikas N Kumar <vikasnkumar@gmail.com>
2011-01-22 23:02:09 -0500

    Can now read section headers correctly. need to find symbols first. updating API.

--- include/dyldo.h
+++ include/dyldo.h
@@ -19,10 +19,36 @@
 
 #include <dyldo_config.h>
 
-void *dyldo_takeout();
+typedef union {
+	enum {
+		PTR_IS_32BIT,
+		PTR_IS_64BIT
+	} type;
+	union u_ptr32or64_t {
+		void *pv;
+		uint32_t *p32;
+		uint64_t *p64;
+	} c;
+} ptr32or64_t;
 
-int dyldo_insert(void *dyldo, pid_t pid, const char *dll, const char *symbol, void *arg);
+typedef struct dyldo_is_opaque dyldo_t;
 
-void dyldo_putback(void *dyldo);
+/* create the dyldo object for the running process whose PID is given as an
+ * argument. Returns a pointer to an opaque object that must be freed by
+ * dyldo_delete() function later to conserve memory.
+ */
+dyldo_t *dyldo_create(pid_t);
+/* delete memory and close all open handles related to the dyldo'ed process.
+ * This can lead to the dyldo'ed process to be unstable if not done in the same
+ * thread as create function above.
+ */
+void dyldo_destroy(dyldo_t *dyldo);
+/* finds the symbol in the symbol table of executable and returns the memory
+ * location of it. On a 64-bit system the running process can be 32 or 64 bit,
+ * and hence they both need to be handled correctly or even simultaneously.
+ */
+ptr32or64_t *dyldo_read_symbol(dyldo_t *, const char *symbol);
+
+int dyldo_insert(dyldo_t *dyldo, const char *dll, const char *symbol, void *arg);
 
 #endif /* __DYLDO_H__ */
--- src/dyldo.c
+++ src/dyldo.c
@@ -18,34 +18,53 @@
 #include <dyldo.h>
 
 #define OS_MAX_BUFFER 512
-#define ERROR_OUT_OF_MEMORY do { \
+#define LOG_ERROR_INVALID_PID(A) do { \
+	fprintf(stderr, "[%s:%d] Invalid PID: %d\n", __func__, __LINE__, A); \
+} while (0)
+#define LOG_ERROR_OUT_OF_MEMORY do { \
 	int err = errno; \
 	fprintf(stderr, "[%s:%d] Out of memory. Error: %s\n", __func__, __LINE__, strerror(err)); \
 } while (0)
-#define ERROR_FILE_OPEN(FF) do { \
+#define LOG_ERROR_FILE_OPEN(FF) do { \
 	int err = errno; \
 	fprintf(stderr, "[%s:%d] File(%s) open error. Error: %s\n", __func__, __LINE__, FF, strerror(err)); \
 } while (0)
-#define ERROR_FILE_SEEK do { \
+#define LOG_ERROR_FILE_SEEK do { \
 	int err = errno; \
 	fprintf(stderr, "[%s:%d] File seek error. Error: %s\n", __func__, __LINE__, strerror(err)); \
 } while (0)
-#define ERROR_FILE_READ do { \
+#define LOG_ERROR_FILE_READ do { \
 	int err = errno; \
 	fprintf(stderr, "[%s:%d] File read error. Error: %s\n", __func__, __LINE__, strerror(err)); \
 } while (0)
-#define ERROR_UNSUPPORTED_PROCESSOR do { \
+#define LOG_ERROR_UNSUPPORTED_PROCESSOR do { \
 	fprintf(stderr, \
 	"[%s:%d] Only 32/64-bit Intel X86/X86-64 processors are supported.\n", __func__, __LINE__); \
 } while (0)
+#define LOG_INFO_HEADERS_LOADED do { \
+	fprintf(stderr, "[%s:%d] Exe headers loaded.\n", __func__, __LINE__); \
+} while (0)
 
-struct dyldo {
-	int inserted;
-	char *exename;
+struct dyldo_is_opaque {
+	pid_t pid;
+	enum {
+		DYLDO_EXE_IS_NEITHER,
+		DYLDO_EXE_IS_32BIT,
+		DYLDO_EXE_IS_64BIT
+	} is64;
 	int fd_exe;
+	void *proghdrs; /* program headers */
+	size_t proghdrnum;
+	size_t proghdrsize; /* total buffer size */
+	void *sechdrs; /* section headers */
+	size_t sechdrnum;
+	size_t sechdrsize; /* total buffer size */
+	size_t secnametblidx;
+	char *exepath;
+	int inserted;
 };
 
-int exe_open_file(pid_t pid)
+static int exe_open_file(pid_t pid)
 {
 	int fd = -1;
 	if (pid > 0) {
@@ -54,34 +73,34 @@ int exe_open_file(pid_t pid)
 		snprintf(buf, sizeof(buf), "/proc/%d/exe", pid);
 		fd = open(buf, O_RDONLY);
 		if (fd < 0) {
-			ERROR_FILE_OPEN(buf);
+			LOG_ERROR_FILE_OPEN(buf);
 		}
 	}
 	return fd;
 }
 
-int exe_elf_identify(unsigned char *e_ident, size_t size)
+static int exe_elf_identify(unsigned char *e_ident, size_t size)
 {
 	if (e_ident && size > 0) {
 		if ((e_ident[EI_MAG0] == ELFMAG0) &&
 			(e_ident[EI_MAG1] == ELFMAG1) &&
 			(e_ident[EI_MAG2] == ELFMAG2) &&
 			(e_ident[EI_MAG3] == ELFMAG3)) {
-			int is64 = -1;
+			int is64 = DYLDO_EXE_IS_NEITHER;
 			/* magic number says this is an ELF file */
 			switch (e_ident[EI_CLASS]) {
 			case ELFCLASS32:
-				is64 = 0;
+				is64 = DYLDO_EXE_IS_32BIT;
 				break;
 			case ELFCLASS64:
-				is64 = 1;
+				is64 = DYLDO_EXE_IS_64BIT;
 				break;
 			case ELFCLASSNONE:
 			default:
-				is64 = -1;
+				is64 = DYLDO_EXE_IS_NEITHER;
 				break;
 			}
-			if (is64 >= 0) {
+			if (is64 != DYLDO_EXE_IS_NEITHER) {
 				int isbigendian = -1;
 				int iscurrent = 0;
 				int islinux = 0;
@@ -109,137 +128,202 @@ int exe_elf_identify(unsigned char *e_ident, size_t size)
 			}
 		}
 	}
-	return -1;
+	return DYLDO_EXE_IS_NEITHER;
 }
 
-int exe_headers32(int fd)
+
+static int exe_load_headers(dyldo_t *dy)
 {
-	Elf32_Ehdr hdr;
-	Elf32_Shdr *sechdrs = NULL;
-	memset(&hdr, 0, sizeof(hdr));
+	Elf32_Ehdr hdr32;
+	Elf64_Ehdr hdr64;
+	off_t shdroffset = 0;
+	off_t phdroffset = 0;
+	int fd = -1;
+	if (!dy) {
+		return -1;
+	}
+	fd = dy->fd_exe;
+	memset(&hdr32, 0, sizeof(hdr32));
 	if (lseek(fd, 0, SEEK_SET) < 0) {
-		ERROR_FILE_SEEK;
+		LOG_ERROR_FILE_SEEK;
 		return -1;
 	}
-	if (read(fd, &hdr, sizeof(hdr)) < 0) {
-		ERROR_FILE_READ;
+	if (read(fd, &hdr32, sizeof(hdr32)) < 0) {
+		LOG_ERROR_FILE_READ;
 		return -1;
 	}
-	if (exe_elf_identify(hdr.e_ident, EI_NIDENT) == 1) {
-		/* this is a 64-bit file. so return */
-		return 1;
-	}
-	if (hdr.e_machine != EM_386) {
-		ERROR_UNSUPPORTED_PROCESSOR;
-		return -1;
-	}
-	if (hdr.e_shnum > 0) {
-		sechdrs = malloc(hdr.e_shentsize * hdr.e_shnum);
-		if (!sechdrs) {
-			ERROR_OUT_OF_MEMORY;
+	dy->is64 = exe_elf_identify(hdr32.e_ident, EI_NIDENT);
+	switch (dy->is64) {
+	case DYLDO_EXE_IS_32BIT:
+		if (hdr32.e_machine != EM_386) {
+			LOG_ERROR_UNSUPPORTED_PROCESSOR;
 			return -1;
 		}
-		memset(sechdrs, 0, hdr.e_shentsize * hdr.e_shnum);
-		do {
-			if (lseek(fd, hdr.e_shoff, SEEK_SET) < 0) {
-				ERROR_FILE_SEEK;
-				break;
-			}
-			if (read(fd, sechdrs, hdr.e_shnum * hdr.e_shentsize) < 0) {
-				ERROR_FILE_READ;
-				break;
-			}
-		} while (0);
-		free(sechdrs);
-		sechdrs = NULL;
+		if (hdr32.e_shoff > 0) {
+			shdroffset = 0 + hdr32.e_shoff;
+			dy->sechdrnum = 0 + hdr32.e_shnum;
+			dy->sechdrsize = 0 + hdr32.e_shnum * hdr32.e_shentsize;
+			dy->secnametblidx = 0 + hdr32.e_shstrndx;
+		}
+		if (hdr32.e_phoff > 0) {
+			phdroffset = 0 + hdr32.e_phoff;
+			dy->proghdrnum = 0 + hdr32.e_phnum;
+			dy->proghdrsize = 0 + hdr32.e_phnum * hdr32.e_phentsize;
+		}
+		break;
+	case DYLDO_EXE_IS_64BIT:
+		memset(&hdr64, 0, sizeof(hdr64));
+		if (lseek(fd, 0, SEEK_SET) < 0) {
+			LOG_ERROR_FILE_SEEK;
+			return -1;
+		}
+		if (read(fd, &hdr64, sizeof(hdr64)) < 0) {
+			LOG_ERROR_FILE_READ;
+			return -1;
+		}
+		if (hdr64.e_machine != EM_X86_64) {
+			LOG_ERROR_UNSUPPORTED_PROCESSOR;
+			return -1;
+		}
+		if (hdr64.e_shoff > 0) {
+			shdroffset = 0 + hdr64.e_shoff;
+			dy->sechdrnum = 0 + hdr64.e_shnum;
+			dy->sechdrsize = 0 + hdr64.e_shnum * hdr64.e_shentsize;
+			dy->secnametblidx = 0 + hdr64.e_shstrndx;
+		}
+		if (hdr64.e_phoff > 0) {
+			phdroffset = 0 + hdr64.e_phoff;
+			dy->proghdrnum = 0 + hdr64.e_phnum;
+			dy->proghdrsize = 0 + hdr64.e_phnum * hdr64.e_phentsize;
+		}
+		break;
+	case DYLDO_EXE_IS_NEITHER:
+	default:
+		return -1;
+	}
+	if (shdroffset > 0 && dy->sechdrsize > 0) {
+		dy->sechdrs = malloc(dy->sechdrsize);
+		if (!dy->sechdrs) {
+			LOG_ERROR_OUT_OF_MEMORY;
+			return -1;
+		}
+		memset(dy->sechdrs, 0, dy->sechdrsize);
+		if (lseek(fd, shdroffset, SEEK_SET) < 0) {
+			LOG_ERROR_FILE_SEEK;
+			return -1;
+		}
+		if (read(fd, dy->sechdrs, dy->sechdrsize) < 0) {
+			LOG_ERROR_FILE_READ;
+			return -1;
+		}
+	}
+	if (phdroffset > 0 && dy->proghdrsize > 0) {
+		dy->proghdrs = malloc(dy->proghdrsize);
+		if (!dy->proghdrs) {
+			LOG_ERROR_OUT_OF_MEMORY;
+			return -1;
+		}
+		memset(dy->proghdrs, 0, dy->proghdrsize);
+		if (lseek(fd, phdroffset, SEEK_SET) < 0) {
+			LOG_ERROR_FILE_SEEK;
+			return -1;
+		}
+		if (read(fd, dy->proghdrs, dy->proghdrsize) < 0) {
+			LOG_ERROR_FILE_READ;
+			return -1;
+		}
 	}
 	return 0;
 }
 
-int exe_headers64(int fd)
+static int exe_get_symboltable(dyldo_t *dy)
 {
-	Elf64_Ehdr hdr;
-	Elf64_Shdr *sechdrs = NULL;
-	memset(&hdr, 0, sizeof(hdr));
-	if (lseek(fd, 0, SEEK_SET) < 0) {
-		ERROR_FILE_SEEK;
+	Elf32_Shdr *shdr32 = NULL;
+	Elf64_Shdr *shdr64 = NULL;
+	if (!dy || !dy->sechdrs) {
 		return -1;
 	}
-	if (read(fd, &hdr, sizeof(hdr)) < 0) {
-		ERROR_FILE_READ;
-		return -1;
-	}
-	if (hdr.e_machine != EM_X86_64) {
-		ERROR_UNSUPPORTED_PROCESSOR;
-		return -1;
-	}
-	if (hdr.e_shnum > 0) {
-		sechdrs = malloc(hdr.e_shentsize * hdr.e_shnum);
-		if (!sechdrs) {
-			ERROR_OUT_OF_MEMORY;
-			return -1;
+	switch(dy->is64) {
+	size_t idx = 0;
+	case DYLDO_EXE_IS_32BIT:
+		shdr32 = (Elf32_Shdr *)dy->sechdrs;
+		for (idx = 0; idx < dy->sechdrnum; ++idx) {
+			if (shdr32[idx].sh_type == SHT_SYMTAB) {
+			}
 		}
-		memset(sechdrs, 0, hdr.e_shentsize * hdr.e_shnum);
-		do {
-			if (lseek(fd, hdr.e_shoff, SEEK_SET) < 0) {
-				ERROR_FILE_SEEK;
-				break;
-			}
-			if (read(fd, sechdrs, hdr.e_shnum * hdr.e_shentsize) < 0) {
-				ERROR_FILE_READ;
-				break;
-			}
-		} while (0);
-		free(sechdrs);
-		sechdrs = NULL;
+		break;
+	case DYLDO_EXE_IS_64BIT:
+		shdr64 = (Elf64_Shdr *)dy->sechdrs;
+		for (idx = 0; idx < dy->sechdrnum; ++idx) {
+
+		}
+		break;
+	case DYLDO_EXE_IS_NEITHER:
+	default:
+		return -1;
 	}
 	return 0;
 }
 
-int exe_get_headers(int fd)
+dyldo_t *dyldo_create(pid_t pid)
 {
-	int is64 = 0;
-	if (fd < 0) {
-		return -1;
-	}
-	is64 = exe_headers32(fd);
-	if (is64 > 0) {
-		return exe_headers64(fd);
-	}
-	return is64;
-}
-
-void *dyldo_takeout()
-{
-	struct dyldo *dy = NULL;
-	dy = malloc(sizeof(*dy));
-	if (dy) {
-		dy->inserted = 0;
+	dyldo_t *dy = NULL;
+	if (pid > 0) {
+		dy = malloc(sizeof(*dy));
+		if (dy) {
+			memset(dy, 0, sizeof(*dy));
+			dy->pid = pid;
+			dy->is64 = DYLDO_EXE_IS_NEITHER;
+			dy->fd_exe = exe_open_file(dy->pid);
+			if (dy->fd_exe > 0) {
+				if (exe_load_headers(dy) >= 0) {
+					LOG_INFO_HEADERS_LOADED;
+				}
+			}
+		} else {
+			LOG_ERROR_OUT_OF_MEMORY;
+		}
 	} else {
-		ERROR_OUT_OF_MEMORY;
+		LOG_ERROR_INVALID_PID(pid);
 	}
 	return dy;
 }
 
-void dyldo_putback(void *dy)
+void dyldo_destroy(dyldo_t *dy)
 {
 	if (dy) {
+		if (dy->fd_exe > 0) {
+			close(dy->fd_exe);
+			dy->fd_exe = -1;
+		}
+		if (dy->sechdrs) {
+			free(dy->sechdrs);
+			dy->sechdrs = NULL;
+		}
+		if (dy->proghdrs) {
+			free(dy->proghdrs);
+			dy->proghdrs = NULL;
+		}
 		free(dy);
 		dy = NULL;
 	}
 }
 
-int dyldo_insert(void *dy_in, pid_t pid, const char *dll, const char *symbol,
+ptr32or64_t *dyldo_read_symbol(dyldo_t *dy, const char *symbol)
+{
+	ptr32or64_t *ptr = NULL;
+	if (!dy || !symbol) {
+		return NULL;
+	}
+	exe_get_symboltable(dy);
+	return ptr;
+}
+
+int dyldo_insert(dyldo_t *dy, const char *dll, const char *symbol,
 				void *arg)
 {
-	struct dyldo *dy = dy_in;
-	if (!dy || pid <= 0) {
+	if (!dy) {
 		return -1;
 	}
-	dy->fd_exe = exe_open_file(pid);
-	if (dy->fd_exe < 0) {
-		return -1;
-	}
-	exe_get_headers(dy->fd_exe);
 	return 0;
 }

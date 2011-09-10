a5370921307c70e2520f13639ca817b9d5d6395f
Vikas N Kumar <vikas@cpan.org>
2011-09-10 10:21:22 -0400

    testing brk() using system calls.

--- test/dummy.c
+++ test/dummy.c
@@ -1,8 +1,11 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <string.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
+#include <sys/syscall.h>
 
 static int counter = 0;
 void myfun()
@@ -15,7 +18,16 @@ void myfun()
 
 int main()
 {
-	printf("Starting dummy\n");
+	intptr_t here0 = 0;
+	intptr_t here1 = 0;
+	const char *str = "Hello World!";
+	size_t len = strlen(str);
+	here0 = (intptr_t)syscall(SYS_brk, 0);
+	here1 = (intptr_t)syscall(SYS_brk, here0 + len + 1);
+	printf("Starting dummy 0x%lx 0x%lx\n", here0, here1);
+	memcpy((void *)here0, str, len + 1);
+	printf("String: %s\n", (const char *)here0);
+	syscall(SYS_brk, here0);
 	while (1) {
 		struct timeval tv = { 0 };
 		sleep(2);
